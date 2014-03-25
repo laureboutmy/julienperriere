a6d82a08f74cb30f5f32ac8af2729aa208c19974
Laure Boutmy <laureboutmy@gmail.com>
2014-03-25 22:04:30 +0100

    form.php, merci @antoinewattier

--- form.php
+++ form.php
@@ -1,7 +1,7 @@
 <?php
-	if(!empty($_POST)) {
-		var_dump($_POST)
-
+	header('Content-Type: application/json');
+	if(isset($_POST) && !empty($_POST)) {
+		
 		$name = $_POST['name'];
 		$email = $_POST['email'];
 		$message = $_POST['message'];
@@ -9,24 +9,19 @@
 		$subject = 'Hi there!';
 		$content= '
 		<html>
-	      	<head>
-	      		<title>' . $subject . '</title>
-	      	</head>
-	      	<body>
-	      		<strong>From: </strong>' . $name . ' <' . $email .'>
-	      		<br /><br /><strong>Message: </strong>' . $message . '
-	      	</body>
-	     </html>
-	    ';
-	    
-		
+	    <head>
+				<title>' . $subject . '</title>
+	    </head>
+	    <body>
+	     	<strong>From: </strong>' . $name . ' <' . $email .'>
+	      <br /><br /><strong>Message: </strong>' . $message . '
+	    </body>
+	  </html>';
 		$mailheader = 'MIME-Version: 1.0' . "\r\n";
 	  $mailheader .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 	  $mailheader .= 'From:' . $email . "\r\n";
 
 		mail($to, $subject, $content, $mailheader) or die('Error!');
-		return true;
-		
-	}
-
+		echo json_encode(true);	
+	} else { echo json_encode(false); }
 ?>
--- scripts/views/infos.js
+++ scripts/views/infos.js
@@ -29,26 +29,42 @@ define([
 
         document.querySelector('button[type="submit"]').addEventListener('click', function(e){
           e.preventDefault();
+          console.log('submit');
           var form = this.parentNode,
-              name = form.querySelector('input[name="name"]'),
-              email = form.querySelector('input[name="email"]'),
-              message = form.querySelector('textarea'),
+              name = form.querySelector('input[name="name"]').value,
+              email = form.querySelector('input[name="email"]').value,
+              message = form.querySelector('textarea').value,
               error = false;
-
-          if(email.value === '' || name.value === '' || message.value === '' || /\S+@\S+\.\S+/.test(email.value)){ error = true; }
+          console.log(name, email, message);
+          if(email == '' || name == '' || message == '' || !/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){ error = true; }
           if(error){
+
             form.querySelector('span.message').innerHTML = 'Whoops! That\'s a no-go.';
             helpers.addClass(form.querySelector('span.message'), 'visible');
           } else {
-            var data = {
-                name: name.value,
-                email: email.value,
-                message: message.value
-              };
+            var data = new FormData();
+            data.append('name', name);
+            data.append('email', email);
+            data.append('message', message);
+            console.log(data); 
             var request = new XMLHttpRequest();
             request.open('POST', 'form.php', true);
-            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+            request.onreadystatechange = function(){
+              if(request.readyState == 4){
+                var response = JSON.parse(request.responseText);
+                if(response){
+                  form.querySelector('span.message').innerHTML = 'Yay! Thanks for your message.';
+                  helpers.addClass(form.querySelector('span.message'), 'visible');
+                } else {
+                  form.querySelector('span.message').innerHTML = 'Whoops! That\'s a no-go.';
+                  helpers.addClass(form.querySelector('span.message'), 'visible');
+                }
+              }
+            }
+            // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
             request.send(data);
+                       
+
           }
         });
       },
